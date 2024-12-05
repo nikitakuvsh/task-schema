e2ee6962b08b8d7980e11dd737ee1908ff88f4e9
nikitakuvsh <nikita_kuvshinnikov@inbox.ru>
2024-12-06 00:01:41 +0300

    otkat

--- src/components/TaskManager/Block.jsx
+++ src/components/TaskManager/Block.jsx
@@ -2,18 +2,7 @@ import React, { useState } from "react";
 
 function Block({ block, index, onMouseDown }) {
     const [nameTask, setNameTask] = useState("");
-    const [showMenu, setShowMenu] = useState(false);
-
-    const handleRightClick = (e) => {
-        e.preventDefault();
-        setShowMenu(true);
-    };
-
-    const handleOptionClick = (option) => {
-        console.log(`Selected option: ${option}`);
-        setShowMenu(false);
-    };
-
+    
     return (
         <div
             id={`block-${index}`}
@@ -28,19 +17,6 @@ function Block({ block, index, onMouseDown }) {
             onContextMenu={handleRightClick}
         >
             <h3 className="block__title">{nameTask || `Блок ${index + 1}`}</h3>
-            {showMenu && (
-                <ul
-                    className="block__menu"
-                    style={{
-                        top: `${block.y + block.height + 10}px`,
-                        left: `${block.x}px`,
-                    }}
-                >
-                    <li onClick={() => handleOptionClick("Редактировать")}>Редактировать</li>
-                    <li onClick={() => handleOptionClick("Удалить")}>Удалить</li>
-                    <li onClick={() => handleOptionClick("Подробнее")}>Подробнее</li>
-                </ul>
-            )}
         </div>
     );
 }
---
+++ src/components/TaskManager/TimeLine.jsx
@@ -0,0 +1,17 @@
+// Timeline.js
+import React from "react";
+
+function Timeline({ startDate, endDate }) {
+    return (
+        <div className="task-manager__timeline-wrapper">
+            <span className="task-manager__timeline timeline-start">
+                {startDate.toLocaleString()}
+            </span>
+            <span className="task-manager__timeline timeline-end">
+                {endDate.toLocaleString()}
+            </span>
+        </div>
+    );
+}
+
+export default Timeline;
