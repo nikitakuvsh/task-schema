8e4ae7f78e58cf8737491013cc9e98300d9ced84
nikitakuvsh <nikita_kuvshinnikov@inbox.ru>
2024-12-09 01:02:04 +0300

    remove space

--- src/components/TaskManager/TimeLine.jsx
+++ src/components/TaskManager/TimeLine.jsx
@@ -4,12 +4,8 @@ import React from "react";
 function Timeline({ startDate, endDate }) {
     return (
         <div className="task-manager__timeline-wrapper">
-            <span className="task-manager__timeline timeline-start">
-                {startDate.toLocaleString()}
-            </span>
-            <span className="task-manager__timeline timeline-end">
-                {endDate.toLocaleString()}
-            </span>
+            <span className="task-manager__timeline timeline-start">{startDate.toLocaleString()}</span>
+            <span className="task-manager__timeline timeline-end">{endDate.toLocaleString()}</span>
         </div>
     );
 }
